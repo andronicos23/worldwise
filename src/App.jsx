322858de4936a5e3186c8a41d7223053b785e2c4
andronicos23 <andronicos.nicolaides@gmail.com>
2024-12-02 14:43:06 +0200

    changes to navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -41,7 +41,7 @@ function App() {
         <Route path="app" element={<AppLayout />}>
           <Route
             index
-            element={<CityList cities={cities} isLoading={isLoading} />}
+            element={<Navigate replace to="cities" isLoading={isLoading} />}
           />
           <Route
             path="cities"
