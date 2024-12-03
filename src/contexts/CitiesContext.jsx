b2b832d5e18ba153472d7838b1aeae6489c3f825
andronicos23 <andronicos.nicolaides@gmail.com>
2024-12-03 16:39:32 +0200

    custom hook useCities

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 
 const BASE_URL = "http://localhost:8000";
 const CitiesContext = createContext();
@@ -35,4 +35,10 @@ function CitiesProvider({ children }) {
   );
 }
 
-export { CitiesProvider };
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined) throw new Error("contect outside of scope");
+  return context;
+}
+
+export { useCities, CitiesProvider };
